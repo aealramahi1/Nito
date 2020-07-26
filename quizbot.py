427412f28013b3a28d68be972edf5549624bba87
aealramahi1 <aealramahi1@gmail.com>
2020-07-26 12:20:46 -0500

    added credit
    Included the discord.py API license as instructed and edited the quizbot.py script to give credit to the tutorials that aided me heavily in writing the code. The quizbot.py script is mostly finished.

--- quizbot.py
+++ quizbot.py
@@ -1,3 +1,8 @@
+# The load, unload, and reload commands used below (along with the for loop) were all derived from a Lucas YouTube tutorial
+# The idea for accessing the bot token through a .env file (and the implementation of this feature) were taken from a web tutorial
+# Lucas YouTube channel: https://www.youtube.com/channel/UCR-zOCvDCayyYy1flR5qaAg/featured
+# Web tutorial: https://realpython.com/how-to-make-a-discord-bot-python/#creating-a-bot
+
 import discord
 import os
 from discord.ext import commands
@@ -6,29 +11,29 @@ from dotenv import load_dotenv # allows us to load the token without displaying 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') # load in the token from the .env file in the same folder
-client = commands.Bot(command_prefix = 'q!') # COMMENT HERE
+bot = commands.Bot(command_prefix = 'q!') # COMMENT HERE
 
-@client.command()
+@bot.command()
 async def load(ctx, extension):
     '''Allows us to load in a cog from within the Discord guild. This makes it easier to add new features to the bot'''
-    client.load_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
     # all cogs are located within the cogs folder
 
-@client.command()
+@bot.command()
 async def unload(ctx, extension):
     '''Allows us to unlaod a cog from within the Discord guild. This will disable any features that the bot may have been
        using from this cog'''
-    client.unload_extension(f'cogs.{extension}')
+    bot.unload_extension(f'cogs.{extension}')
 
-@client.command()
+@bot.command()
 async def reload(ctx, extension):
     '''Unloads and the loads a cog. Useful when the code of a cog is updated'''
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
 
 # the os module will allow us to load in all the cogs we have when our bot goes online
 for filename in os.listdir('./cogs'): # lists all the files in cogs
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}') # strips the last three characters to get rid of the .py
+        bot.load_extension(f'cogs.{filename[:-3]}') # strips the last three characters to get rid of the .py
 
-client.run(TOKEN) # run the client, bringing the bot online
+client.run(TOKEN) # bring the bot online
