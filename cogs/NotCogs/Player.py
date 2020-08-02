357b718fe3882f4e7c1d3f89cbc5264e9d5abcd3
aealramahi1 <aealramahi1@gmail.com>
2020-08-02 14:45:37 -0500

    Updated Player.py
    Added get functions for attributes of the object that will need to be accessed. Learned about snowflake ID's.

--- cogs/NotCogs/Player.py
+++ cogs/NotCogs/Player.py
@@ -5,12 +5,26 @@ class Player:
 {
     def __init__(self, name):
         '''Initializes a Player object for the user that calls this function, stores the name, and initializes the scores to 0'''
-        # we want to make sure that the user's Discord name and discriminator are passed in here
-        if name.contains("#"):
-            self.name = name
-        else:
-            print("An error occurred! Could not initialize Player object. Please report this error :(")
-            ####
-            # perhaps at some point in the future there can be a place where issues for this bot are reported
-            ####
+        # I was originally going to check if the user's Discord name was actually passed in here, but that's not necessary since
+        # the passing will be done by the programmer and not by the user AND because I'm going to be using the user's snowflake ID
+        self.name = name # snowflake ID for the user (this way it doesn't matter if they change their username)
+        self.total_score = 0
+        self.round_score = 0
+        self.question_index = 0 # current question index (questions stored in a list)
+
+    def getName():
+        '''Returns the user's Discord name including the discriminator'''
+        return name
+
+    def getTotalScore():
+        '''Returns the user's total score'''
+        return self.total_score
+
+    def getRoundScore():
+        '''Returns the user's round score'''
+        return self.round_score
+
+    def getQuestionIndex():
+        '''Returns the index of the question that the user is currently on'''
+        return self.question_index
 }
