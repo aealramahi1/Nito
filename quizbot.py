671102fc7f5ad094573e65dfe58def48aa050c2a
aealramahi1 <aealramahi1@gmail.com>
2020-08-03 00:30:25 -0500

    Changed files names and updated quizbot.py
    Changed the name of the .env file to quizbot.py (to steer away from the format of the code that was in the tutorial I followed). Additionally, I updated the quizbot.py file to include my own code instead of tutorial code. New code has not yet been tested so the crucial parts of the old code have simply been commented out instead of removed. If this works, I will have shaved off some lines of code.

--- quizbot.py
+++ quizbot.py
@@ -1,48 +1,48 @@
-# The load, unload, and reload commands used below (along with the for loop) were all derived from a Lucas YouTube tutorial
-# The idea for accessing the bot token through a .env file (and the implementation of this feature) were from a web tutorial
-# Lucas YouTube channel: https://www.youtube.com/channel/UCR-zOCvDCayyYy1flR5qaAg/featured
-# Web tutorial: https://realpython.com/how-to-make-a-discord-bot-python/#creating-a-bot
-
 import discord
-import os
+##import os
 from discord.ext import commands
-from dotenv import load_dotenv # allows us to load the token without displaying it within the code
-# this allows opern-source code to be secure because you cannot access my bot's token
+from bot_token import TOKEN # Allows us to load the token without displaying it within the code
+# This allows opern-source code to be secure because you cannot access my bot's token
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN') # load in the token from the .env file in the same folder
-bot = commands.Bot(command_prefix = 'q!') # here we set the server prefix to q!, meaning that all commands
+bot = commands.Bot(command_prefix = "q!") # Here we set the server prefix to q!, meaning that all commands
 # are called using the format q!cmdname
 
-# in the functions below, we check to see if the user has administrative privileges in the guild so that random
-# server users cannot unload or load cogs and create chaos
+##### in the functions below, we check to see if the user has administrative privileges in the guild so that random
+##### server users cannot unload or load cogs and create chaos
+
+# List of all the cogs that this bot will run by default. When new cogs are created, bots that ran the original code
+# can either re-run this code (I will update this list) or they can use the load and reload functions
+allcogs = ["defaults",
+           "round_creator"]
 
 @bot.event
 async def on_ready():
-    print("Bot is online!")
+    print("Bot successfully connected!")
 
 @bot.command()
-async def load(ctx, extension):
+async def load(ctx, ext):
     '''Allows us to load in a cog from within the Discord guild. This makes it easier to add new features to the bot'''
-    bot.load_extension(f'cogs.{extension}')
-    # all cogs are located within the cogs folder
+    bot.load_extension("cogs.%s" % ext)
+    # All cogs are located within the cogs folder
 
 @bot.command()
-async def unload(ctx, extension):
+async def unload(ctx, ext):
     '''Allows us to unlaod a cog from within the Discord guild. This will disable any features that the bot may have been
        using from this cog'''
-    bot.unload_extension(f'cogs.{extension}')
+    bot.unload_extension("cogs.%s" % ext)
 
 @bot.command()
-async def reload(ctx, extension):
+async def reload(ctx, ext):
     '''Unloads and the loads a cog. Useful when the code of a cog is updated'''
-    bot.unload_extension(f'cogs.{extension}')
-    bot.load_extension(f'cogs.{extension}')
+    bot.reload_extension("cogs.%s" % ext)
 
 if __name__ == "__main__":
-    # the os module will allow us to load in all the cogs we have when our bot goes online
-    for filename in os.listdir('./cogs'): # lists all the files in cogs
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}') # strips the last three characters to get rid of the .py
+##    # The os module will allow us to load in all the cogs we have when our bot goes online
+##    for file in os.listdir("./cogs"): # Lists all the files in cogs
+##        if file.endswith(".py"):
+##            bot.load_extension("cogs.%s" % file[:-3]) # Strips the last three characters to get rid of the .py
 
-bot.run(TOKEN) # bring the bot online
+    for cog in allcogs:
+        bot.load_extension("cogs.%s" % cog)
+
+bot.run(TOKEN) # Bring the bot online
