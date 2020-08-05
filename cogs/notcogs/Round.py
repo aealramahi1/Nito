d57bf3e833f6db88d41c8c2db94043ff1558aac4
aealramahi1 <aealramahi1@gmail.com>
2020-08-04 22:41:16 -0500

    Changed bot name, working on Round.py
    Changed the bot name to NitoBot (which is much more creative than the previous name) since "nito" means exam in Latin. Slowly but surely filling out Round.py

--- cogs/notcogs/Round.py
+++ cogs/notcogs/Round.py
@@ -4,9 +4,15 @@ class Round:
 {
     def __init__(self, thisplayer):
         '''Initializes the values for this object'''
-        self.roundowner = thisplayer # The person who created the round is the only one who can control it
+        self.round_owner = thisplayer # The person who created the round is the only one who can control it
+        self.player_list = [round_owner] # List to contain all the players for this round, including the owner
+        # The round owner is always the first player in the list
         self.question_time = 5
-        self.buzztime = 6
+        self.buzz_time = 6
 
     def add_player(self, newplayer):
+        '''Adds another player to the list of current players'''
+        if newplayer not in self.player_list and \
+        newplayer is not self.round_owner:
+            
 }
