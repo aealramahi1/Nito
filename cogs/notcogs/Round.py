dc0a14972534926d86904fb08a65da005f3bf834
aealramahi1 <aealramahi1@gmail.com>
2020-08-07 12:39:26 -0500

    Updated Round.py
    Added many more functions to Round.py. It's really starting to come together.

--- cogs/notcogs/Round.py
+++ cogs/notcogs/Round.py
@@ -5,17 +5,45 @@ class Round:
     area in which the bot is played
 
     Attributes:
-        round_owner (Player object): Person who creates and controls the round
-        player_list (list): Contains all players for the round
         question_time (float): Seconds in between each sentence
         buzz_time (float): Seconds the user has to guess after buzzing
+        round_owner (Player object): Person who creates and controls the round
+        player_list (list): Contains all players for the round
+        round_status (boolean): True if the round is playing, False if not
 
     Methods:
-        add_player(newplayer): Adds another player to player_list
+        startRound(thisplayer): Starts the round with thisplayer as
+                                 round_owner
+        endRound(): Ends the round and sets variables to null
+        addPlayer(newplayer): Adds another player to player_list
+        removePlayer(oldplayer): Removes a player from player_list
+        setQuestionTime(num): Sets question_time to num
+        resetQuestionTime(): Resets question_time to default (5.0)
+        setBuzzTime(num): Sets buzz_time to num
+        resetBuzzTime(): Resets buzz_time to default (6.0)
+
+        getQuestionTime(): Returns question_time
+        getBuzzTime(): Returns buzz_time
+        getRoundOwner(): Returns round_owner
+        getPlayers(): Returns player_list
+        getRoundStatus(): Returns round_status
     '''
-    def __init__(self, thisplayer):
+    
+    def __init__(self):
         '''
         Initializes the values for this object
+        '''
+        # Time (seconds) in between each sentence of the question
+        self.question_time = 5.0
+        # Time (seconds) the player has to guess after buzzing
+        self.buzz_time = 6.0
+
+    ## SETTERS, SHAKERS, MOVERS, WHAT HAVE YOU
+
+    def startRound(self, thisplayer):
+        '''
+        Starts the round with the player who called this command as the round
+        owner.
 
         Parameters:
             thisplayer (Player object): Person who created the round
@@ -24,12 +52,18 @@ class Round:
         self.round_owner = thisplayer
         # List to contain all the players for this round, including the owner
         self.player_list = [round_owner]
-        # Time (seconds) in between each sentence of the question
-        self.question_time = 5.0
-        # Time (seconds) the player has to guess after buzzing
-        self.buzz_time = 6.0
+        # Indicates whether the round is currently active or not
+        self.round_status = True
 
-    def add_player(self, newplayer):
+    def endRound():
+        '''
+        Ends the round and sets all relevant variables to null
+        '''
+        self.round_owner = None
+        self.player_list = []
+        self.round_status = False
+
+    def addPlayer(self, newplayer):
         '''
         Adds another player to the list of current players
 
@@ -42,8 +76,8 @@ class Round:
         '''
         # Make sure the player is not the owner or already in the list of
         # current players
-        if newplayer not in self.player_list and \
-        newplayer is not self.round_owner:
+        if newplayer not in self.player_list and \ 
+           newplayer is not self.round_owner:
             self.player_list.append(newplayer)
             message = newplayer.getPartialName() + " joined successfully!"
 
@@ -59,7 +93,154 @@ class Round:
         # client code if the user is a valid player, but just in case it
         # doesn't hurt to include an else statement
         else:
-            message = "An error occurred :("
+            message = "Somehow, an error occurred :("
 
         return message
+
+    def removePlayer(self, oldplayer):
+        '''
+        Removes a current player from the list of players
+
+        Parameters:
+            oldplayer (Player object): The player who wishes to leave
+
+        Returns:
+            message (str): The message detailing the results of the function
+                           (i.e. whether it worked or not)
+        '''
+        # Make sure the player is already in the list of players, but not the
+        # owner
+        if oldplayer in self.player_list and \
+           newplayer is not self.round_owner:
+            self.player_list.remove(oldplayer)
+            message = oldplayer.getPartialName() + " has quit."
+
+        # Check to see if the player is the round owner
+        elif oldplayer is self.round_owner:
+            endRound()
+            message = "The round owner has quit. Round ended."
+
+        # Check to see if the user wasn't even playing
+        elif oldplayer not in self.player_list:
+            message = "You're not even playing, how can you quit???"
+
+        # Let's hope this doesn't run...
+        else:
+            message = "I don't know how, but you triggered an error..."
+
+        return message
+
+    def setQuestionTime(num):
+        '''
+        Changes question_time
+
+        Parameters:
+            num (float/int): The time to set question_time to
+
+        Returns:
+            message (str): The message detailing the results of the function
+                           (i.e. whether it worked or not)
+        '''
+        # Prevent negative time
+        if num < 0:
+            message = "You cannot have negative time"
+
+        # Pretty reasonable numbers for now
+        elif 0 <= num and num <= 60:
+            self.question_time = float(num) # question_time is always a float
+            message = "Question time set to: " + num
+        
+        # Prevent super big numbers
+        elif num > 60:
+            message = "That number is way too big"
+            
+        # Hopefully this never runs, but you can't be too safe
+        else:
+            message = "An error has occurred"
+
+        return message
+
+    def resetQuestionTime():
+        '''
+        Resets question_time to default
+
+        Returns:
+            message (str): Lets us know that this function works properly
+        '''
+        self.question_time = 5.0
+        message = "Question time reset to 5.0 seconds"
+        return message
+
+    def setBuzzTime(num):
+        '''
+        Changes buzz_time
+
+        Parameters:
+            num (float/int): The time to set buzz_time to
+
+        Returns:
+            message (str): The message detailing the results of the function
+                           (i.e. whether it worked or not)
+        '''
+        # Prevent negative time
+        if num < 0:
+            message = "You cannot have negative time"
+
+        # Pretty reasonable numbers for now
+        elif 0 <= num and num <= 60:
+            self.buzz_time = float(num)
+            message = "Question time set to: " + num
+        
+        # Prevent super big numbers
+        elif num > 60:
+            message = "That number is way too big"
+            
+        # Hopefully this never runs, but you can't be too safe
+        else:
+            message = "An error has occurred"
+
+        return message
+
+    def resetBuzzTime():
+        '''
+        Resets buzz_time to default
+
+        Returns:
+            message (str): Lets us know that this function works properly
+        '''
+        self.buzz_time = 6.0
+        message = "Buzz time reset to 6.0 seconds"
+        return message
+
+    ## GETTERS
+
+    def getQuestionTime():
+        '''
+        Returns question_time
+        '''
+        return question_time
+
+    def getBuzzTime():
+        '''
+        Returns buzz_time
+        '''
+        return buzz_time
+
+    def getRoundOwner():
+        '''
+        Returns round_owner
+        '''
+        return round_owner
+
+    def getPlayers():
+        '''
+        Returns player_list
+        '''
+        return player_list
+
+    def getRoundStatus():
+        '''
+        Returns round_status
+        '''
+        return round_status
 }
